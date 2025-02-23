8c853fc83f62e7c2f4b0c2a10db75d8691549d92
Luiz Neto <luiz@lvtec.com.br>
2025-02-23 19:34:54 -0300

    improve navbar

--- components/nav-projects.tsx
+++ components/nav-projects.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { type LucideIcon } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 import {
   SidebarGroup,
@@ -23,6 +23,7 @@ export function NavProjects({
 }) {
   const { isMobile, setOpen } = useSidebar();
   const router = useRouter();
+  const pathname = usePathname();
 
   const handleClick = (url: string) => {
     router.push(url);
@@ -35,7 +36,12 @@ export function NavProjects({
       <SidebarMenu>
         {projects.map((item) => (
           <SidebarMenuItem key={item.name}>
-            <SidebarMenuButton asChild>
+            <SidebarMenuButton
+              asChild
+              className={`hover:bg-primary/10 ${
+                pathname === item.url ? "bg-primary/10" : ""
+              }`}
+            >
               <button onClick={() => handleClick(item.url)}>
                 <item.icon />
                 <span>{item.name}</span>
